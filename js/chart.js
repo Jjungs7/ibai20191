21d80bb545d6a5165f42bff465be2fe2cbc9e041
jjungs <jeongy95@naver.com>
2019-06-08 22:22:30 +0900

    updated chart

--- js/chart.js
+++ js/chart.js
@@ -2,11 +2,51 @@
 let header_bottom = document.getElementById('footer').offsetTop;
 let footer_top = document.getElementById('header').getBoundingClientRect().height;
 let chart = document.getElementById('chart');
-let stock_list = document.getElementsByClassName('chart-section')[0];
-let stock_list_section = stock_list.firstElementChild;
-let stock_list_section_top = stock_list_section.getBoundingClientRect().height;
-chart.style.height = (header_bottom - stock_list_section_top - footer_top).toString() + 'px';
+let chart_section = document.getElementsByClassName('chart-section')[0];
+let chart_section_first = chart_section.firstElementChild;
+let chart_section_first_top = chart_section_first.getBoundingClientRect().height;
+let stock_list = document.getElementsByClassName('stock-list')[0];
 stock_list.style.height = (header_bottom - footer_top).toString() + 'px';
+chart.style.height = (header_bottom - chart_section_first_top - footer_top).toString() + 'px';
+chart_section.style.height = (header_bottom - footer_top).toString() + 'px';
+
+let newChartFunction = undefined;
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.stock-list p').click(() => {
+    newChartFunction();
+  });
+});
+
+function changeList(type) {
+  let domestic = document.getElementById('domestic');
+  let foreign = document.getElementById('foreign');
+  if(type === 'domestic') {
+    domestic.style.display = 'block';
+    foreign.style.display = 'none';
+  } else if(type === 'foreign') {
+    domestic.style.display = 'none';
+    foreign.style.display = 'block';
+  }
+}
+
+document.getElementById('stock-list-group').addEventListener('change', e => {
+  changeList(e.target.value);
+});
+
+document.querySelectorAll('.stock-list ul li').forEach((li, k, arr) => {
+  li.addEventListener('click', e => {
+    newChartFunction();
+    let text = e.target.innerText;
+    /*
+    if(text.length > 5) {
+      text = text.substring(0, 5);
+      text += '...';
+    }
+    */
+    document.getElementById('cur-stock').innerText = text;
+  });
+});
 
 am4core.ready(function() {
 
@@ -33,7 +73,7 @@ am4core.ready(function() {
   series.dataFields.lowValueY = "low";
   series.dataFields.highValueY = "high";
   series.simplifiedProcessing = true;
-  series.tooltipText = "Open:${openValueY.value}\nLow:${lowValueY.value}\nHigh:${highValueY.value}\nClose:${valueY.value}";
+  series.tooltipText = "Open:{openValueY.value} 원\nLow:{lowValueY.value} 원\nHigh:{highValueY.value} 원\nClose:{valueY.value} ";
 
   chart.cursor = new am4charts.XYCursor();
 
@@ -55,17 +95,26 @@ am4core.ready(function() {
 
   let interval = null;
   let lastDate = null;
-  function getRandomFloat(min, max) {
-    return Math.random() * (max - min) + min;
+  function getRandomIntWon(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let val = Math.floor(Math.random() * (max - min + 1)) + min;
+    val = Math.floor(val / 100) * 100;
+    return val;
   }
 
   function getData(formattedDate, baseData) {
+    let min = Math.ceil(-1);
+    let max = Math.floor(1);
+    let val = Math.floor(Math.random() * (max - min + 1)) + min;
+    let range_small = -1000 + (val === -1 ? -1000 : 0);
+    let range_big = 1000 + (val === 1 ? 1000 : 0);
     return {
       'date': formattedDate,
       'open': baseData,
-      'high': baseData + getRandomFloat(-3.0, 3.0),
-      'low': baseData + getRandomFloat(-3.0, 3.0),
-      'close': baseData + getRandomFloat(-3.0, 3.0)
+      'high': baseData + getRandomIntWon(range_small, range_big),
+      'low': baseData + getRandomIntWon(range_small, range_big),
+      'close': baseData + getRandomIntWon(range_small, range_big)
     }
   }
 
@@ -74,17 +123,12 @@ am4core.ready(function() {
       return `${d.getFullYear()}-${('0' + (d.getMonth() + 1).toString()).slice(-2)}-${('0' + d.getDate().toString()).slice(-2)}`;
     };
 
-    let getDateDiff = (date1, date2) => {
-      const diffTime = Math.abs(date2.getTime() - date1.getTime());
-      return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    };
-
     let sd = new Date(startDate);
     let ed = new Date(endDate);
 
     let newData = [];
     let formattedDate = toCustomFormat(sd);
-    let baseData = 130.00;
+    let baseData = 10000;
     while(formattedDate <= toCustomFormat(ed)) {
       let data = getData(formattedDate, baseData);
       newData.push(data);
@@ -107,6 +151,17 @@ am4core.ready(function() {
     };
 
     chart.data = generateData('2019-05-01', '2019-06-13');
+    let lastData = chart.data[chart.data.length - 1];
+    let start = document.getElementById('cur-stock-start');
+    let high = document.getElementById('cur-stock-highest');
+    let low = document.getElementById('cur-stock-lowest');
+    let end = document.getElementById('cur-stock-end');
+
+    start.innerText = lastData.open;
+    high.innerText = lastData.high;
+    low.innerText = lastData.low;
+    end.innerText = lastData.close;
+
     interval = setInterval(() => {
       let newData = chart.data;
       let data = getData(toCustomFormat(lastDate), chart.data[chart.data.length - 1].close);
@@ -114,14 +169,14 @@ am4core.ready(function() {
       newData.splice(0, 1);
       chart.data = newData;
       lastDate = new Date(lastDate.setDate(lastDate.getDate() + 1));
+
+      start.innerText = data.open;
+      high.innerText = data.high;
+      low.innerText = data.low;
+      end.innerText = data.close;
     }, 10000);
   }
 
   toNewChart();
-
-  document.addEventListener('DOMContentLoaded', () => {
-    $('.stock-list p').click(() => {
-      toNewChart();
-    });
-  });
+  newChartFunction = toNewChart;
 });
